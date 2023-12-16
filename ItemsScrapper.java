61722ddb10db295cd612b526ccdd06c54eecbfba
curdette <150437994+curdette@users.noreply.github.com>
2023-12-16 22:27:28 +0100

    Le prix ItemsScrapper.java

--- ItemsScrapper.java
+++ ItemsScrapper.java
@@ -36,14 +36,14 @@ public final class ItemsScrapper {
 
         // Titre sans espace avant 
 
-        Character a = titre.charAt(1);        
-        while(a==(' ')){
-          titre = titre.substring(1,titre.length());
-          a = titre.charAt(1);
-          System.out.println(titre);
-        }
-        titre = titre.substring(1,titre.length());
-        
+        //*Character a = titre.charAt(1);        
+        //while(a==(' ')){
+         // titre = titre.substring(1,titre.length());
+         // a = titre.charAt(1);
+        //}
+        //titre = titre.substring(1,titre.length());
+        //
+
         //Auteur
 
         int balise_A=pageSource.indexOf("product__manufacturer",fin_A)+"product__manufacturer".length()+2;
@@ -59,13 +59,34 @@ public final class ItemsScrapper {
 
         String etat=pageSource.substring(debut_E,fin_E);
 
-        // Physique_Audio
+        //Prix
+        int debut_P=0; //comme on a deux debut different, celui ci sera le debut final
+        int debut_P1=pageSource.indexOf("product__price",fin_P)+"product__price".length()+2; //debut possilble de prix 1
+        int debut_P2=pageSource.indexOf("--sale",fin_P)+"--sale".length()+2; //debut possible de prix 2
+        int teste=debut_P1+1; //(javais pas d'idée de nom de variable), c'est pour avoir l'idx du caractere juste apres le debut_1
+        String cs1=pageSource.substring(debut_P1, teste); //sert a avoir le caractere juste apres en string
+        int idxcs1=pageSource.indexOf(cs1); //jPour avoir l'idx du string que je viens de créer, si je met teste ça marche pas
+        Character charcs1=pageSource.charAt(idxcs1); //je le transforme en caractere pour pouvoir l'analyser 
 
-        int debut_P=pageSource.indexOf("product__type",fin_P)+"product__type".length()+2;
-        fin_P=pageSource.indexOf("<", debut_P);
+        
+        if(Character.isDigit(charcs1)){ //ici je vérifie que me caractere est un chiffre, le premier chiffre du prix 
+           debut_P=debut_P1;//si c'est un chiffre alors j'ai le bon début
+        }
+        else {
+          debut_P=debut_P2;
+          //Sinon c'est l'autre
+        }
 
-        String audio=pageSource.substring(debut_P,fin_P);
+      fin_P=pageSource.indexOf("&",debut_P); 
+      String ps=pageSource.substring(debut_P, fin_P);
+      Double prix= Double.parseDouble(ps.replace(',', '.')); //le changer de String a Double en remplacer la ',' par '.'
 
+        //Audi ou Physique
+
+        int debut_Au=pageSource.indexOf("product__type",fin_P)+"product__type".length()+2;
+        fin_P=pageSource.indexOf("<", debut_Au);
+
+        String audio=pageSource.substring(debut_Au,fin_P);
         
 
 
@@ -74,7 +95,12 @@ public final class ItemsScrapper {
 
         
         
-      /* Idee : 
+        
+
+
+      // add code here
+      /* Idee :
+      etat après <div class="product__condition"> 
       Physique_Audio après <span class="product__type">
       Prix après <span class="product__price">
       
@@ -93,12 +119,12 @@ public final class ItemsScrapper {
         Item item = new Item();
         item.setTitre(titre);
         item.setAuteur(auteur);
-        item.setPrix(null);
+        item.setPrix(prix);
         item.setEtat(etat);
         item.setPhysique_Audio(audio);
         itemList.add(item);
 
-        if (i==149){          // A changer quand on a recup toutes les données 
+        if (i==100){          // A changer quand on a recup toutes les données 
           // on a fini d'extraire les item
           break;
           }
