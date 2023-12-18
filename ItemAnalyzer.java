767766cc41b65de259fb3861d899569c7ca649b3
shezza75 <150438238+shezza75@users.noreply.github.com>
2023-12-18 14:06:52 +0100

    A continuer

--- ItemAnalyzer.java
+++ ItemAnalyzer.java
@@ -3,6 +3,7 @@ package fr.pantheonsorbonne.ufr27.miashs.poo;
 import java.lang.Double;
 import java.lang.Integer;
 import java.lang.String;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public final class ItemAnalyzer {
@@ -13,18 +14,55 @@ public final class ItemAnalyzer {
   }
 
   public Double getEcart_Type_Prix() {
-    // code here
-    return null;
+    Double vraitem = 0.;
+    double moyenne = 0.;
+    double variance = 0.;
+    for(Item item : items){
+      vraitem =  item.getPrix();
+  
+      moyenne += vraitem ;
+      
+    }
+    
+    moyenne = (moyenne-vraitem)/146;
+    moyenne = Math.round(moyenne*100.0)/100.0;
+    //System.out.println("c'est la moyenne apres div "+moyenne);
+    for(Item item : items){
+      variance +=Math.pow((item.getPrix()-moyenne),2)/146;
+    }
+    //System.out.println(moyenne);
+    //System.out.println(variance);
+    Double ecartype = Math.sqrt(variance);
+    //System.out.println(ecartype);
+    //DecimalFormat decimalFormat = new DecimalFormat("#.##");
+    //double ecartype_arr = Double.parseDouble(decimalFormat.format(ecartype));
+    double ecartype_arr = Math.round(ecartype*100.0)/100.0;
+    
+    return ecartype_arr;
   }
 
-  public String getAuteurs_Differents() {
-    // code here
-    return null;
+  public Integer getAuteurs_Differents() {
+    int c=0;
+    String listAuteur ="";
+    for(Item item : items){
+      String vraitem = item.getAuteur();
+      if(listAuteur.indexOf(vraitem)==-1){
+        listAuteur+=vraitem;
+        c++;
+      }
+    }
+    return c;
   }
 
   public Integer getNb_Lettres_Moyens_Arr() {
-    // code here
-    return null;
+    String listitre = "";
+    for(Item item : items){
+      
+      String vraitem = item.getTitre();
+      listitre += vraitem;
+    }
+    listitre = listitre.replace(" ", "");
+    return  (int) Math.ceil(listitre.length()/146);
   }
 
   public ArrayList<Double> getMoyenne_Prix_Etat() {
@@ -33,6 +71,7 @@ public final class ItemAnalyzer {
   }
 
   public Double getPart_Livre_Audio() {
+
     // code here
     return null;
   }
