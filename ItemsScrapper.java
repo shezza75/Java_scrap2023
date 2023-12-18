44b2d151845f30c35d64f5491ecb2805c348c140
shezza75 <150438238+shezza75@users.noreply.github.com>
2023-12-18 10:12:19 +0100

    Final version, pas toucher (sauf commentaire quand meme)

--- ItemsScrapper.java
+++ ItemsScrapper.java
@@ -19,9 +19,10 @@ public final class ItemsScrapper {
       int fin_A =  0;
       int fin_E = 0;
       int fin_P=0;
+      int fin_Prix=0;
       while (true) {
         
-        // Titre 
+        //////////////////////////////////////////   Titre 
 
         int debut_T=pageSource.indexOf("product__title",fin_T)+"product__title".length()+2;
         int option1_T=pageSource.indexOf("(", debut_T);
@@ -34,17 +35,17 @@ public final class ItemsScrapper {
         }
         String titre = pageSource.substring(debut_T, fin_T);
 
-        // Titre sans espace avant 
+        ///////////  Titre sans espace avant 
 
-        //*Character a = titre.charAt(1);        
-        //while(a==(' ')){
-         // titre = titre.substring(1,titre.length());
-         // a = titre.charAt(1);
-        //}
-        //titre = titre.substring(1,titre.length());
-        //
+        Character a_T = titre.charAt(1);        
+        while(a_T==(' ')){
+          titre = titre.substring(1,titre.length());
+          a_T = titre.charAt(1);
+          //System.out.println(titre);
+        }
+        titre = titre.substring(1,titre.length());
 
-        //Auteur
+        // ////////////////////////////////////////   Auteur
 
         int balise_A=pageSource.indexOf("product__manufacturer",fin_A)+"product__manufacturer".length()+2;
         int debut_A=pageSource.indexOf(">", balise_A);
@@ -52,17 +53,29 @@ public final class ItemsScrapper {
         
         String auteur = pageSource.substring(debut_A+1, fin_A);
 
-        //Etat
+        ////////////////////////////////////////////  Etat
 
         int debut_E=pageSource.indexOf("product__condition",fin_E)+"product__condition".length()+2;
         fin_E=pageSource.indexOf("<", debut_E);
 
         String etat=pageSource.substring(debut_E,fin_E);
 
-        //Prix
-        int debut_P=0; //comme on a deux debut different, celui ci sera le debut final
-        int debut_P1=pageSource.indexOf("product__price",fin_P)+"product__price".length()+2; //debut possilble de prix 1
-        int debut_P2=pageSource.indexOf("--sale",fin_P)+"--sale".length()+2; //debut possible de prix 2
+        /////////// Enlever les espaces des etats 
+
+        Character a_E = etat.charAt(1);
+        while(a_E==(' ')){
+          etat = etat.substring(1,etat.length());
+          a_E=etat.charAt(1);
+          System.out.println(etat);
+        } 
+        etat = etat.substring(1,etat.length());
+
+
+        //////////// Prix 
+        
+        int debut_Prix=0; //comme on a deux debut different, celui ci sera le debut final
+        int debut_P1=pageSource.indexOf("product__price",fin_Prix)+"product__price".length()+2; //debut possilble de prix 1
+        int debut_P2=pageSource.indexOf("--sale",fin_Prix)+"--sale".length()+2; //debut possible de prix 2
         int teste=debut_P1+1; //(javais pas d'idée de nom de variable), c'est pour avoir l'idx du caractere juste apres le debut_1
         String cs1=pageSource.substring(debut_P1, teste); //sert a avoir le caractere juste apres en string
         int idxcs1=pageSource.indexOf(cs1); //jPour avoir l'idx du string que je viens de créer, si je met teste ça marche pas
@@ -70,52 +83,30 @@ public final class ItemsScrapper {
 
         
         if(Character.isDigit(charcs1)){ //ici je vérifie que me caractere est un chiffre, le premier chiffre du prix 
-           debut_P=debut_P1;//si c'est un chiffre alors j'ai le bon début
+           debut_Prix=debut_P1;//si c'est un chiffre alors j'ai le bon début
         }
         else {
-          debut_P=debut_P2;
+          debut_Prix=debut_P2;
           //Sinon c'est l'autre
         }
 
-      fin_P=pageSource.indexOf("&",debut_P); 
-      String ps=pageSource.substring(debut_P, fin_P);
+        
+      fin_P=pageSource.indexOf("&",debut_Prix); 
+      String ps=pageSource.substring(debut_Prix, fin_P);
       Double prix= Double.parseDouble(ps.replace(',', '.')); //le changer de String a Double en remplacer la ',' par '.'
 
-        //Audi ou Physique
-
-        int debut_Au=pageSource.indexOf("product__type",fin_P)+"product__type".length()+2;
-        fin_P=pageSource.indexOf("<", debut_Au);
-
-        String audio=pageSource.substring(debut_Au,fin_P);
         
 
 
 
 
+        //////////////////////////////////////////  Physique_Audio
 
-        
-        
-        
+        int debut_P=pageSource.indexOf("product__type",fin_P)+"product__type".length()+2;
+        fin_P=pageSource.indexOf("<", debut_P);
 
-
-      // add code here
-      /* Idee :
-      etat après <div class="product__condition"> 
-      Physique_Audio après <span class="product__type">
-      Prix après <span class="product__price">
-      
-      Je ne sais pas si on aura d'autre info, comment ne pas recuperer que la première donnée ?
-      --> Il faut spécifier a quel item nous avons à faire
-
-      Si on veut se placer on peut faire un comptage de substring
-      faire une boucle pour faire un substring i fois pour être bien placé
-
-
-
-      */
-        
-        
-        
+        String audio=pageSource.substring(debut_P,fin_P);
+ 
         Item item = new Item();
         item.setTitre(titre);
         item.setAuteur(auteur);
@@ -124,7 +115,7 @@ public final class ItemsScrapper {
         item.setPhysique_Audio(audio);
         itemList.add(item);
 
-        if (i==100){          // A changer quand on a recup toutes les données 
+        if (i==148){          // PASSSS 149 pitié sinon marche pas
           // on a fini d'extraire les item
           break;
           }
